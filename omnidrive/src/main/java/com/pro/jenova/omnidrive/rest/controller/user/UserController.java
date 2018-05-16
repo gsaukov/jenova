5c27a401acfb8b01142d59e31fcbd5d60ddc8c60
someone <someone@somewhere.com>
2018-05-16 21:47:42 +0200

    int

--- omnidrive/src/main/java/com/pro/jenova/omnidrive/rest/controller/user/UserController.java
+++ omnidrive/src/main/java/com/pro/jenova/omnidrive/rest/controller/user/UserController.java
@@ -1,22 +1,23 @@
 package com.pro.jenova.omnidrive.rest.controller.user;
 
-import com.pro.jenova.omnidrive.data.repository.UserRepository;
+import com.pro.jenova.omnidrive.rest.controller.configuration.AppConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private AppConfiguration appConfiguration;
 
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public RestUser register(@RequestBody RestUser restUser) {
-        return new RestUser.Builder().withUsername(restUser.getUsername()).withPassword(restUser.getPassword()).build();
+    @RequestMapping(value = "/technical", method = RequestMethod.GET)
+    public RestUser technical() {
+        return new RestUser.Builder().withUsername(appConfiguration.getUsername()).withPassword(appConfiguration.getPassword()).build();
     }
 
 }
