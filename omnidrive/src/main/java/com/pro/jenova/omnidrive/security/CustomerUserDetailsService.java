6ba65d907d9a4f00e286d87c853e89e010888bf1
someone <someone@somewhere.com>
2018-08-08 23:19:13 +0200

    fix

--- omnidrive/src/main/java/com/pro/jenova/omnidrive/security/CustomerUserDetailsService.java
+++ omnidrive/src/main/java/com/pro/jenova/omnidrive/security/CustomerUserDetailsService.java
@@ -1,18 +1,24 @@
 package com.pro.jenova.omnidrive.security;
 
+import org.slf4j.Logger;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import static org.slf4j.LoggerFactory.getLogger;
 import static org.springframework.security.core.authority.AuthorityUtils.createAuthorityList;
 
 @Service
 public class CustomerUserDetailsService implements UserDetailsService {
 
+    private static final Logger logger = getLogger(CustomerUserDetailsService.class);
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.info("Attempting to load user by username [{}].");
+
         return new User("dimitrios", "password", createAuthorityList("MANAGE_USER"));
     }
 
