b1cdf2b11bf16aea72e5bafb2ac89afcd4ec8559
someone <someone@somewhere.com>
2018-08-10 22:20:04 +0200

    fix

--- omnidrive/src/main/java/com/pro/jenova/omnidrive/security/CustomerUserDetailsService.java
+++ omnidrive/src/main/java/com/pro/jenova/omnidrive/security/CustomerUserDetailsService.java
@@ -1,13 +1,11 @@
 package com.pro.jenova.omnidrive.security;
 
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import static org.slf4j.LoggerFactory.getLogger;
