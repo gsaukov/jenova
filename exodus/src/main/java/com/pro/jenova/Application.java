0ab391924f978fdaf123bab76210c9cea533aeb4
someone <someone@somewhere.com>
2019-02-06 21:44:00 +0100

    fix

--- exodus/src/main/java/com/pro/jenova/Application.java
+++ exodus/src/main/java/com/pro/jenova/Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import static org.springframework.boot.WebApplicationType.SERVLET;
 
 @EnableFeignClients
+@EnableWebMvc
 @EnableEurekaClient
 @SpringBootApplication
 public class Application {
