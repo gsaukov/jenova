315c2a7f664debe7720c5890378d13758e2b83ed
someone <someone@somewhere.com>
2019-02-13 22:04:20 +0100

    fix

--- justitia/src/main/java/com/pro/jenova/justitia/rest/controller/approval/ApprovalController.java
+++ justitia/src/main/java/com/pro/jenova/justitia/rest/controller/approval/ApprovalController.java
@@ -3,17 +3,13 @@ package com.pro.jenova.justitia.rest.controller.approval;
 import com.pro.jenova.common.rest.ErrorResponse;
 import com.pro.jenova.common.rest.RestResponse;
 import com.pro.jenova.common.rest.VoidResponse;
-import com.pro.jenova.justitia.data.entity.User;
-import com.pro.jenova.justitia.data.repository.UserRepository;
+import com.pro.jenova.justitia.rest.controller.approval.request.RestRevokeApprovalRequest;
 import com.pro.jenova.justitia.rest.controller.approval.response.RestListApprovalDetails;
 import com.pro.jenova.justitia.rest.controller.approval.response.RestListApprovalsResponse;
-import com.pro.jenova.justitia.rest.controller.user.request.RestCreateUserRequest;
-import com.pro.jenova.justitia.rest.controller.user.request.RestRemoveUserRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
@@ -26,35 +22,22 @@ public class ApprovalController {
     private static final SimpleDateFormat FORMATTER = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ");
 
     @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @PostMapping("/create")
-    public ResponseEntity<RestResponse> create(@RequestBody RestCreateUserRequest restCreateUserRequest) {
-        if (userRepository.existsByUsername(restCreateUserRequest.getUsername())) {
-            return ErrorResponse.badRequest("USERNAME_ALREADY_EXISTS");
-        }
+    @PostMapping("/revoke")
+    public ResponseEntity<RestResponse> remove(@RequestBody RestRevokeApprovalRequest restRevokeApprovalRequest) {
 
-        userRepository.save(new User.Builder()
-                .withUsername(restCreateUserRequest.getUsername())
-                .withPassword(passwordEncoder.encode(restCreateUserRequest.getPassword()))
-                .build());
+        // Uses JdbcTemplate's batchUpdate operation to bulk load data
+        int result = jdbcTemplate.update(
+                "delete from oauth_approvals where clientId = ? and userId = ? and scope = ?",
+                restRevokeApprovalRequest.getClientId(), restRevokeApprovalRequest.getUserId(),
+                restRevokeApprovalRequest.getScope());
 
-        return VoidResponse.created();
-    }
-
-    @PostMapping("/remove")
-    public ResponseEntity<RestResponse> remove(@RequestBody RestRemoveUserRequest restRemoveUserRequest) {
-        if (userRepository.removeByUsername(restRemoveUserRequest.getUsername()) > 0L) {
+        if (result > 0) {
             return VoidResponse.ok();
         }
 
-        return ErrorResponse.badRequest("USERNAME_NOT_FOUND");
+        return ErrorResponse.badRequest("APPROVAL_NOT_FOUND");
     }
 
     @GetMapping("/list")
