75980e921ba924e054b5cc290432073537ab7bb6
someone <someone@somewhere.com>
2019-03-09 00:32:21 +0100

    fix

--- common-data/src/main/java/com/pro/jenova/common/data/audit/DataSourceAuditListener.java
+++ common-data/src/main/java/com/pro/jenova/common/data/audit/DataSourceAuditListener.java
@@ -4,21 +4,28 @@ import com.pro.jenova.common.util.audit.AuditContext;
 import net.ttddyy.dsproxy.ExecutionInfo;
 import net.ttddyy.dsproxy.QueryInfo;
 import net.ttddyy.dsproxy.listener.logging.AbstractQueryLoggingListener;
+import org.slf4j.Logger;
 
 import java.util.List;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class DataSourceAuditListener extends AbstractQueryLoggingListener {
+
+    private static final Logger logger = getLogger(DataSourceAuditListener.class);
 
     @Override
     public void afterQuery(ExecutionInfo execInfo, List<QueryInfo> queryInfoList) {
+        super.afterQuery(execInfo, queryInfoList);
+
         AuditContext auditContext = AuditContext.DIAGNOSTICS.get();
-        auditContext.incDbQueriesCount(queryInfoList.size());
         auditContext.incDbTimeMillis(execInfo.getElapsedTime());
+        auditContext.incDbQueriesCount(1L);
     }
 
     @Override
     protected void writeLog(String message) {
-        // NOP
+        logger.debug(message);
     }
 
 }
