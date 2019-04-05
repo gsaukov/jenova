5a4a21716bc04c32352b3b96d97ec1da96ab8a9c
someone <someone@somewhere.com>
2019-04-06 00:17:24 +0200

    fix

--- common-rest/src/main/java/com/pro/jenova/common/rest/client/LogCorrelationIdInterceptor.java
+++ common-rest/src/main/java/com/pro/jenova/common/rest/client/LogCorrelationIdInterceptor.java
@@ -1,5 +1,6 @@
 package com.pro.jenova.common.rest.client;
 
+import org.slf4j.Logger;
 import org.slf4j.MDC;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
@@ -10,13 +11,20 @@ import java.io.IOException;
 
 import static com.pro.jenova.common.util.LogCorrelation.LOG_CORRELATION_ID;
 import static com.pro.jenova.common.util.LogCorrelation.useExistingOrCreateNew;
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class LogCorrelationIdInterceptor implements ClientHttpRequestInterceptor {
+
+    private static final Logger logger = getLogger(LogCorrelationIdInterceptor.class);
 
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
             throws IOException {
-        request.getHeaders().add(LOG_CORRELATION_ID, useExistingOrCreateNew(MDC.get(LOG_CORRELATION_ID)));
+        String logCorrelationId = useExistingOrCreateNew(MDC.get(LOG_CORRELATION_ID));
+
+        logger.debug("Adding logCorrelationId {} into headers for outgoing client request.", logCorrelationId);
+
+        request.getHeaders().add(LOG_CORRELATION_ID, logCorrelationId);
         return execution.execute(request, body);
     }
 
