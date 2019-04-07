66d0c372a5a9f3404a7ca388ce097127bdca28af
someone <someone@somewhere.com>
2019-04-07 12:54:12 +0200

    security

--- omnidrive/src/main/java/com/pro/jenova/omnidrive/security/ResourceServerConfigurer.java
+++ omnidrive/src/main/java/com/pro/jenova/omnidrive/security/ResourceServerConfigurer.java
@@ -1,6 +1,5 @@
 package com.pro.jenova.omnidrive.security;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,9 +8,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -21,15 +17,6 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter {
-
-    @Value("${security.oauth2.resource.client-id}")
-    private String clientId;
-
-    @Value("${security.oauth2.resource.client-secret}")
-    private String clientSecret;
-
-    @Value("${security.oauth2.resource.token-info-uri}")
-    private String tokenInfoUri;
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -44,35 +31,22 @@ public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
-        resources.tokenServices(delegatingTokenServices());
+        resources.tokenServices(defaultTokenServices());
     }
 
     @Bean
-    public ResourceServerTokenServices delegatingTokenServices() {
-        DelegatingTokenServices delegatingTokenServices = new DelegatingTokenServices();
-        delegatingTokenServices.setRemoteTokenServices(remoteTokenServices());
-        delegatingTokenServices.setDefaultTokenServices(defaultTokenServices());
-        return delegatingTokenServices;
-    }
-
-    private ResourceServerTokenServices remoteTokenServices() {
-        RemoteTokenServices tokenServices = new RemoteTokenServices();
-        tokenServices.setCheckTokenEndpointUrl(tokenInfoUri);
-        tokenServices.setClientId(clientId);
-        tokenServices.setClientSecret(clientSecret);
-        return tokenServices;
-    }
-
-    private ResourceServerTokenServices defaultTokenServices() {
+    private DefaultTokenServices defaultTokenServices() {
         DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
         defaultTokenServices.setTokenStore(tokenStore());
         return defaultTokenServices;
     }
 
-    private TokenStore tokenStore() {
+    @Bean
+    private JwtTokenStore tokenStore() {
         return new JwtTokenStore(accessTokenConverter());
     }
 
+    @Bean
     private JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
         converter.setSigningKey("123");
