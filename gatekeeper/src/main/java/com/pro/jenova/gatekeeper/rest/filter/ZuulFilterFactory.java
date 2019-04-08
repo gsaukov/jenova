c2dd6a23699955965010ac0828e8a64100530e3c
someone <someone@somewhere.com>
2019-04-08 23:07:34 +0200

    fix

--- gatekeeper/src/main/java/com/pro/jenova/gatekeeper/rest/filter/ZuulFilterFactory.java
+++ gatekeeper/src/main/java/com/pro/jenova/gatekeeper/rest/filter/ZuulFilterFactory.java
@@ -3,10 +3,10 @@ package com.pro.jenova.gatekeeper.rest.filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+
 public class ZuulFilterFactory {
 
-    @Bean
+
     public BearerHeaderFilter bearerHeaderFilter() {
         return new BearerHeaderFilter();
     }
