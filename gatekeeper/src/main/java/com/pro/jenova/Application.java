8ea977231dbea442217ce690fb418610c9fe60ab
someone <someone@somewhere.com>
2019-04-08 23:51:11 +0200

    fix

--- gatekeeper/src/main/java/com/pro/jenova/Application.java
+++ gatekeeper/src/main/java/com/pro/jenova/Application.java
@@ -1,7 +1,6 @@
 package com.pro.jenova;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -10,7 +9,7 @@ import static org.springframework.boot.WebApplicationType.SERVLET;
 
 @EnableZuulProxy
 @EnableEurekaClient
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
